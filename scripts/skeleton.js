df1c5aa49d43ed617c2495f806b678025d15d561
Matthew Chartier <MChartier@gmail.com>
2012-03-19 18:55:10 -0400

    preparing to pull from Olga; working on company page stuff...

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -1,3 +1,5 @@
+// Main page -------------------------------------------------------------------
+
 function openSearch() {
   openMain();
   $("#searchbar").show();
@@ -31,7 +33,9 @@ function openMain() {
   $(".homebutton").removeClass("ui-btn-active");
 }
 
-function openInfo() {
+// Career page -----------------------------------------------------------------
+
+function openCareerInfo() {
   $(".careerpagecontent").hide();
   $("#infocontent").show();
 }
@@ -50,6 +54,70 @@ function openHistory() {
   $(".careerpagecontent").hide();
   $("#historycontent").show();
 }
+
+// Company pages ---------------------------------------------------------------
+
+function getCompanyPage(companyId) {
+  return $("#companypage" + companyId);
+}
+
+function isCompanyOpen(companyId) {
+  return (getCompanyPage(companyId).length > 0);
+}
+
+// 'opening' a company means creating a page for said company in the DOM
+// and then navigating to it
+function openCompany(companyId) {
+  // if company page already exists, display error message and do nothing
+  if(isCompanyOpen(companyId)) {
+    alert('ERROR: Tried to open company page for company with ID ' +
+	  companyId + ', but company page already exists.');
+    return;
+  }
+
+  // Credit to Mack Staples of securityandcaffeine.com for this trick.
+  // Copy of template page is created with all the nice JQM formatting.
+  // Upon initialization, we'll asynchronously populate all of the 
+  // company-specific info!
+  var companyPage = $("#companytemplate").clone();
+  
+  companyPage.attr('id', 'companypage' + companyId);
+  companyPage.attr('data-companyid', companyId);
+
+  // add the new page to the DOM
+  companyPage.appendTo('body');
+
+  $.mobile.changePage(companyPage);
+}
+
+// 'closing' a company means cleaning up that company's page in the DOM
+// This will help to prevent the document object model from getting overly 
+// large and slowing things down.
+function closeCompany(companyId) {
+  getCompanyPage(companyId).remove();
+}
+
+function openCompanyInfo() {
+  $(".companypagecontent").hide();
+  $("#companyinfocontent").show();
+}
+
+function openCompanyReviews() {
+  $(".companypagecontent").hide();
+  $("#companyreviewscontent").show();
+}
+
+function openCompanyContacts() {
+  $(".companypagecontent").hide();
+  $("#companycontactscontent").show();
+}
+
+function openCompanyInterviews() {
+  $(".companypagecontent").hide();
+  $("#companyinterviewscontent").show();
+}
+
+// Page load script ------------------------------------------------------------
 
 $('#main').live('pageinit', function(event) {
   // hide content we don't need to see yet
@@ -103,14 +171,13 @@ $('#main').live('pageinit', function(event) {
       blankContent : "<p>Options for how to filter the graph will go here.</p>"
     })
   });
-
 });
 
 $('#career').live('pageinit', function(event) {
-  openInfo();
+  openCareerInfo();
 
   // set up event handlers
-  $("#infobutton").click(openInfo);
+  $("#infobutton").click(openCareerInfo);
   $("#xpbutton").click(openXP);
   $("#favoritesbutton").click(openFavorites);
   $("#historybutton").click(openHistory);
@@ -128,4 +195,28 @@ $('#career').live('pageinit', function(event) {
   footer.css("z-index", "1");
   footer.css("position", "absolute");
   footer.css("bottom", "0");
+});
+
+$('#companytemplate').live('pageinit', function(event) {
+  openCompanyInfo();
+
+  // set up event handlers
+  $("#companyinfobutton").click(openCompanyInfo);
+  $("#companyreviewsbutton").click(openCompanyReviews);
+  $("#companycontactsbutton").click(openCompanyContacts);
+  $("#companyinterviewsbutton").click(openCompanyInterviews);
+
+  $(".homebutton").click(openMain);
+
+  // position navigational elements
+  // var header = $("#companyheader");
+  // var footer = $("#companyfooter");
+  // var contentpages = $(".companypagecontent");
+
+  // header.css("z-index", "1");
+  // contentpages.css("position", "absolute");
+  // contentpages.css("top", "64px");
+  // footer.css("z-index", "1");
+  // footer.css("position", "absolute");
+  // footer.css("bottom", "0");
 });
